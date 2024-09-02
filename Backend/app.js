6835dc1592eb17c858439f8462e35729078e9bd2
Rubezsingla <124330683+Rubezsingla@users.noreply.github.com>
2024-09-02 17:10:08 +0530

    d
    d

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
 const connectDB = require('./src/Database/db');
-
 const cardRoutes = require('./src/Routes/cards.Routes');
+const userRoutes = require('./src/Routes/users.Routes'); // Import user routes
 
 const app = express();
 
@@ -13,7 +14,16 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+// Session setup
+app.use(session({
+    secret: 'your_secret_key', // Replace with a secure secret key
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false } // Set to true if using HTTPS
+}));
+
 // Routes
 app.use('/cards', cardRoutes);
+app.use('/users', userRoutes); // Add user-related routes
 
 module.exports = app;
